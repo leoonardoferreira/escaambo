bd2ebda1afa316f73c0bc69728dc970bcb13b194
Leo <ferreiradasilvaleonardo7@gmail.com>
2024-04-12 21:13:48 -0300

    criei uma aplicação react, por enquanto, o projeto em react está "sem utilidade", mas em breve o html e o css como arquivos devem sumir e só haverá arquivos js para criar o app inteiro

---
+++ web/src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Home() {
+    return(
+        <h1>só organizando e criando o projeto em react para fazer alterações no futuro</h1>
+    );
+}
+
+export default Home;
