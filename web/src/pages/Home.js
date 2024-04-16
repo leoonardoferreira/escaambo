3c07fb9a0f8cf852455f2c14174b95382202dcfb
Leo <ferreiradasilvaleonardo7@gmail.com>
2024-04-16 11:00:41 -0300

    deenvolvemos o comeco do site
    Co-authored-by: Maria Kassandra  <mariaksks@users.noreply.github.com>

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -1,8 +1,22 @@
 import React from "react";
+import styled from "styled-components";
 
 function Home() {
     return(
-        <h1>só organizando e criando o projeto em react para fazer alterações no futuro</h1>
+        <>
+            <header>
+                <a href="login.html">Cadastre-se</a>
+                <div class="headline">
+                    <h1><span>aprenda</span> algo novo com <span>pessoas reais</span> de forma <span>gratuita</span></h1>
+                    <img className="logo" src="assets/logo.svg" alt=""/>
+                </div>
+            </header>
+            <main>
+               <section>
+                   </section> 
+            </main>
+        </>
+    
     );
 }
 
