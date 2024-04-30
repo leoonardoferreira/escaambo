22887eba2069dc335a72fb628b6c86b8ab0e739a
Leo <ferreiradasilvaleonardo7@gmail.com>
2024-04-30 11:06:14 -0300

    criei a pagina inicial da aplicacação ( quando o usuário faz login) e seu css na raiz do projeto (ou seja, fora do projeto react)

--- web/src/pages/Login.js
+++ web/src/pages/Login.js
@@ -1,15 +1,88 @@
 import React from "react";
 import styled from "styled-components";
 
+const LoginContainer = styled.div`
+    display: grid;
+    place-items: center;
+    
+    height: 100vh;
+    
+    .login {
+        max-width: 28rem;
+        align-items: center;
+        
+        border-radius: 20px;
+        padding: 2rem 1.5rem;
+        
+        border: .5px var(--grey) solid;
+        
+        transition: all .25s;
+        
+        box-shadow: 0px 3px 7px 0 var(--grey);
+    }
+
+    h1 {
+        color: var(--text-black);
+        font-size: 1.25rem;
+        margin-bottom: 4rem;
+    }
+
+    input {
+        border-radius: 15px;
+        border: none;
+        
+        width: 100%;
+        padding: 1.25rem;
+        margin-bottom: 3rem;
+        
+        border: 1px solid var(--grey);
+        
+    }
+
+    input::placeholder {
+        color: var(--grey);
+        font-weight: 500;
+        font-size: 1rem;
+        font-family: 'Inter', sans-serif;
+    }
+
+    button {
+        width: 100%;
+        padding: 1rem 0;
+        margin-bottom: 1.5rem;
+        
+        text-align: center;
+        font-size: 1rem;
+        font-weight: 600;
+        
+        border: 0;
+        
+        border-radius: 10px;
+        
+        background-color: #00ec99;
+    }
+
+    button:hover {
+        cursor: pointer;
+    }
+
+    a {
+        color: var(--text-black);
+        font-weight: bold;
+    }
+`;
+
 function Login() {
     return(
-        <>
-            <h1>Login</h1>
-            <input class="email" type="email" aria-label="E-mail" placeholder="E-mail"/>
-            <input type="password" aria-label="Senha" placeholder="Senha"/>    
-            <button type="submit" value="Entrar">Entrar</button>
-            <a href="#">Esqueci minha senha</a>
-        </>
+        <LoginContainer>
+            <div className="login">
+                <h1>Login</h1>
+                <input class="email" type="email" aria-label="E-mail" placeholder="E-mail"/>
+                <input type="password" aria-label="Senha" placeholder="Senha"/>    
+                <button type="submit" value="Entrar">Entrar</button>
+                <a href="#">Esqueci minha senha</a>
+            </div>
+        </LoginContainer>
     )
 }
 
